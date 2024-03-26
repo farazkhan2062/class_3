cc386457c46f03091f4bd77872b0287d295aea00
farazkhan2062 <farazkhan2062@gmail.com>
2024-03-26 20:59:15 +0500

    add files for first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+function square(num) {
+    console.log(num * num);
+}
+square(3);
+square(8);
---
+++ main.ts
@@ -0,0 +1,7 @@
+function square(num : number){
+    console.log(num*num);
+}
+
+square(3);
+square(8);
+
